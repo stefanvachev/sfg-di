6b8c8721eb01544b30c75eb424617fd9fc335a70
stefanvachev <vatchev@prodigy.net>
2020-06-13 17:26:11 -0500

    Added tests and controllers, with autowire dependency injection.

---
+++ src/test/java/guru/springframework/sfgdi/controllers/ConstructorInjectedControllerTest.java
@@ -0,0 +1,20 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ConstructorInjectedControllerTest {
+
+    ConstructorInjectedController controller;
+    @BeforeEach
+    void setUp() {
+        controller = new ConstructorInjectedController(new GreetingServiceImpl());
+
+    }
+
+    @Test
+    void getString() {
+        System.out.println(controller.getString());
+    }
+}
